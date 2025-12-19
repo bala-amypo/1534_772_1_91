6b2d3f9dc9979163b3bd8d0151d7fa0dfddac14c
bala-amypo <bala@amypo.in>
2025-12-19 06:16:31 +0000

    User commit

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -3,7 +3,7 @@ package com.example.demo.model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+@Entity
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
