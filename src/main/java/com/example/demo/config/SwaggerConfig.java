ded95ea112df78033c18d5ff510eae138e1f6c8d
bala-amypo <bala@amypo.in>
2025-12-26 05:27:11 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -13,10 +13,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Ticket Management API")
-                        .version("1.0"))
-                        .description("API documentation")
-                .servers(List.of(
-                        new Server().url("https://9058.pro604cr.amypo.ai")
-                ));
+                        .version("1.0")
+                        .description("API documentation"));
     }
 }
