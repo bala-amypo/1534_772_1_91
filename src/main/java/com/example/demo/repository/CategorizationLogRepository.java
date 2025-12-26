39c8a0ce11d511e49ef0f9fdc65e0a424b1cf69d
bala-amypo <bala@amypo.in>
2025-12-26 07:50:57 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationLogRepository.java
+++ src/main/java/com/example/demo/repository/CategorizationLogRepository.java
@@ -2,16 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.CategorizationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface CategorizationLogRepository extends JpaRepository<CategorizationLog, Long> {
 
-    List<CategorizationLog> findByTicket_Id(Long ticketId);
-
-    List<CategorizationLog> findByCategory_Id(Long categoryId);
-
-    List<CategorizationLog> findByRootCause_Id(Long rootCauseId);
+    List<CategorizationLog> findByTicket_Id(Long id);
 }
