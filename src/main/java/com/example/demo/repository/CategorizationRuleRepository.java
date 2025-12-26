fabb50a8603397efe292ad3cd5345e84f7484057
bala-amypo <bala@amypo.in>
2025-12-26 06:27:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
+++ src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
@@ -3,11 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.CategorizationRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CategorizationRuleRepository extends JpaRepository<CategorizationRule, Long> {
-
-    List<CategorizationRule> findAllByOrderByPriorityDesc();
-
-    List<CategorizationRule> findByRootCause_Id(Long rootCauseId);
 }
